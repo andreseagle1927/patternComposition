e30fd393613df585e1a0ddcd8004d0777e10f33f
andreseagle1927 <96150493+andreseagle1927@users.noreply.github.com>
2023-09-27 09:08:39 -0500

    finished before debug

--- src/main/java/dev/abadia/pluggableFactory/ScooterElectricidad.java
+++ src/main/java/dev/abadia/pluggableFactory/ScooterElectricidad.java
@@ -4,8 +4,6 @@ public class ScooterElectricidad extends Scooter
 {
     public void visualizaCaracteristicas()
     {
-        System.out.println("Scooter electrica de modelo: "
-                + modelo + " de color: " + color +
-                " de potencia: " + potencia);
+        System.out.println("Scooter electrica de modelo: "+ modelo + " de color: " + color +" de potencia: " + potencia);
     }
 }
