08d882e5a97d93b3ef116fd41807f5bb63555927
andreseagle1927 <96150493+andreseagle1927@users.noreply.github.com>
2023-09-27 09:03:14 -0500

    arriving to the final

---
+++ src/main/java/dev/abadia/pluggableFactory/AutomovilGasolina.java
@@ -0,0 +1,10 @@
+package dev.abadia.pluggableFactory;
+
+
+public class AutomovilGasolina extends Automovil
+{
+    public void visualizaCaracteristicas()
+    {
+        System.out.println("Automovil de gasolina de modelo: " + modelo +" de color: " + color + " de potencia: " + potencia + " de espacio: " + espacio);
+    }
+}
